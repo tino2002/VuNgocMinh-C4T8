bebe75f0d1b564db4a6f5a65af87758466477ee2
tino2002 <m.vu20@yahoo.com>
2018-09-16 22:39:57 +0700

    bai 2

---
+++ hw.py
@@ -0,0 +1,4 @@
+n = input("nhập bán kính hình tròn : ")
+a = int(n)
+b = a ** 2 * 3.14
+print("Diện tích hình tròn là : ", b)
---
+++ hw2.py
@@ -0,0 +1,4 @@
+n = input("Enter the temperature in Celcius : ")
+a = int(n)
+b = (a * 1.8)+32 
+print(b)
